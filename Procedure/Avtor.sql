0f95db489273d22bb6bbf7d91105619c6458593f
mmmatey <stern.matej@gmail.com>
2012-04-05 23:11:34 +0200

    Tabele in procedure

---
+++ Procedure/Avtor.sql
@@ -0,0 +1,22 @@
+USE [BookMe]
+GO
+
+/****** Object:  Table [dbo].[Avtor]    Script Date: 04/05/2012 23:09:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Avtor](
+	[ID] [int] NOT NULL,
+	[Avtor] [nvarchar](500) NULL,
+	[Variante] [nvarchar](500) NULL,
+ CONSTRAINT [PK_Avtor] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
---
+++ Procedure/Gradivo.sql
@@ -0,0 +1,21 @@
+USE [BookMe]
+GO
+
+/****** Object:  Table [dbo].[Gradivo]    Script Date: 04/05/2012 23:09:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Gradivo](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Vrsta] [nvarchar](200) NULL,
+ CONSTRAINT [PK_Gradivo] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
