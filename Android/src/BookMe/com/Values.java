6eb40e94d77351118e949d93f43ca4819a7bcde6
mmmatey <stern.matej@gmail.com>
2012-06-07 12:25:47 +0200

    finale

--- Android/src/BookMe/com/Values.java
+++ Android/src/BookMe/com/Values.java
@@ -2,5 +2,5 @@ package BookMe.com;
 
 public class Values {
 
-	public static String restURL="http://164.8.118.88/BookMe/Service1.svc/json/";
+	public static String restURL="http://164.8.119.231/BookMe/Service1.svc/json/";
 }
